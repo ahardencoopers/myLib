7e283d9a135e322ac31b8a7f6ab662df45c3b271
achs <ahardencoopers@gmail.com>
2015-05-17 21:31:48 -0500

    Add function to avoid page expiration when clicking the back button.

--- myMisc.php
+++ myMisc.php
@@ -71,4 +71,12 @@ function dumpArreglo($arrDatos)
 	print_r(array_values($arrDatos));
 }
 
+function noExpire()
+{
+		header("Expires: Sat, 01 Jan 2000 00:00:00 GMT");
+		header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+		header("Cache-Control: post-check=0, pre-check=0",false);
+		session_cache_limiter("must-revalidate");
+}
+
 ?>
